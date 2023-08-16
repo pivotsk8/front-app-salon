1da5ea9e7d3301f55433ef192c8d154eb8abbc70
piiivot <the_pivotsk8@hotmail.com>
2023-08-16 09:00:50 -0500

    feat: appointment layout created css and store

--- src/views/appointment/AppointmentsLayout.vue
+++ src/views/appointment/AppointmentsLayout.vue
@@ -3,8 +3,28 @@
 </script>
 
 <template>
-    <div>
-        <h1>AppointmentLayout</h1>
+    <div class="flex justify-between">
+        <h1 class="text-2xl lg:text-6xl font-black text-white">Barber Racing</h1>
+
+        <div class="flex flex-col space-y-5">
+            <div class="flex gap-2 item-center">
+                <p class="text-white text-right">hola: Usario</p>
+
+                <button type="button"
+                    class="bg-red-600 hover:bg-red-700 p-2 text-white uppercase text-xs font-extrabold rounded-lg">Cerrar
+                    Sesión</button>
+            </div>
+
+            <nav class="flex gap-2 item-center justify-end">
+                <button class="p-3 text-gray-200 uppercase text-xs font-black rounded-lg">Mis Citas</button>
+
+                <RouterLink :to="{ name: 'new-appointment' }"
+                    class="p-3 text-gray-200 uppercase text-xs font-black rounded-lg bg-blue-700">
+                    Nueva Cita
+                </RouterLink>
+            </nav>
+
+        </div>
     </div>
     <main>
         <RouterView />

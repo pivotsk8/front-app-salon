410e642cb6e7369ca6811aecc1433e7d11d5c0ca
piiivot <the_pivotsk8@hotmail.com>
2023-09-27 11:28:35 -0500

    feat: auth routing for alls views

---
+++ src/views/auth/AuthLayout.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+const autRoutes = [
+    { name: 'register', text: 'Crear una cuenta' },
+    { name: 'login', text: 'Iniciar Sessión' }
+]
+</script>
+
+<template>
+    <div class="mx-auto lg:w-4/5 mt-20">
+        <RouterView />
+
+        <nav class="mt-10 flex flex-col items-center space-y-5 lg:flex-row lg:justify-between lg:space-y-0">
+            <RouterLink v-for="authRoute in autRoutes" class="uppercase font-bold text-white"
+                :to="{ name: authRoute.name }">{{ authRoute.text }}</RouterLink>
+        </nav>
+    </div>
+</template>
+
---
+++ src/views/auth/LoginView.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+
+<template>
+    <div>
+        <h1>desde login</h1>
+    </div>
+</template>

88d7959c301c22e6834ce574c6e0c572a11cd608
piiivot <the_pivotsk8@hotmail.com>
2023-09-28 11:48:55 -0500

    feat error message when intro wrong info user

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -1,10 +1,44 @@
 <script setup>
+import { inject } from 'vue'
+import AuthApi from '../../api/AuthApi'
 
+const toast = inject('toast')
+
+const handleSubmit = async (formData) => {
+    try {
+        const { data } = await AuthApi.login(formData)
+
+    } catch (error) {
+        toast.open({
+            message: error?.response?.data.msg,
+            type: 'error'
+        })
+    }
+}
 </script>
 
 
 <template>
     <div>
-        <h1>desde login</h1>
+        <h1 class="text-6xl font-extrabold text-white text-center mt-10">Iniciar Sesión</h1>
+        <p class="text-2xl text-white text-center my-5">Si tienes una cuenta, inicia sesión</p>
+
+        <FormKit id="loginForm" type="form" :actions="false"
+            incomplete-message="No se puede enviar, revisar las notificaciones" @submit="handleSubmit">
+
+            <FormKit type="email" label="Email" name="email" placeholder="Email de usuario" validation="required|email"
+                :validation-messages="{
+                    required: 'El Email es obligatorio',
+                    email: 'Email no valido'
+                }" />
+
+            <FormKit type="password" label="Password" name="password" placeholder="Password de Ususario - Min 8 Caracteres"
+                validation="required" :validation-messages="{
+                    required: 'El Password es obligatorio'
+                }" />
+
+            <FormKit type="submit">Iniciar Sesión</FormKit>
+        </FormKit>
+
     </div>
 </template>

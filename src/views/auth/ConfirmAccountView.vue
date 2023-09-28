6e3e0d9d1de0abc22bc481a833017fffdaf73b09
piiivot <the_pivotsk8@hotmail.com>
2023-09-28 11:16:37 -0500

    feat: verify user and push to login

--- src/views/auth/ConfirmAccountView.vue
+++ src/views/auth/ConfirmAccountView.vue
@@ -1,10 +1,38 @@
 <script setup>
+import { inject } from 'vue'
+import { onMounted } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import AuthApi from '../../api/AuthApi'
+
+const toast = inject('toast')
+const route = useRoute()
+const router = useRouter()
+const { token } = route.params
+
+onMounted(async () => {
+    try {
+        const { data } = await AuthApi.verifyAccount(token)
+        toast.open({
+            message: data.msg,
+            type: 'success'
+        })
+        setTimeout(() => {
+            router.push({ name: 'login' })
+        }, 5000);
+
+    } catch (error) {
+        toast.open({
+            message: error.response.data.msg,
+            type: 'error'
+        })
+    }
+})
 
 </script>
 
 
 <template>
     <div>
-        <h1>desde el confirm</h1>
+        <h1 class="text-6xl font-extrabold text-white text-center mt-10">Verificar cuenta</h1>
     </div>
 </template>

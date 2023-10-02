7aaaf4a49730895014a0a9eb7bab53080617c1fc
piiivot <the_pivotsk8@hotmail.com>
2023-10-02 09:50:36 -0500

    feat created store use and fucntion logout

--- src/views/appointment/AppointmentsLayout.vue
+++ src/views/appointment/AppointmentsLayout.vue
@@ -1,36 +1,41 @@
 <script setup>
+import { useUserStore } from '../../stores/user';
 
+const user = useUserStore()
 </script>
 
 <template>
-    <div class="flex justify-between">
-        <h1 class="text-2xl lg:text-6xl font-black text-white">Barber Racing</h1>
+    <div>
+        <div class="flex justify-between">
+            <h1 class="text-2xl lg:text-6xl font-black text-white">Barber Racing</h1>
 
-        <div class="flex flex-col space-y-5">
-            <div class="flex gap-2 item-center">
-                <p class="text-white text-right">hola: Usario</p>
+            <div class="flex flex-col space-y-5">
+                <div class="flex gap-2 item-center">
+                    <p class="text-white text-right">hola: {{ user.getUserName }}</p>
 
-                <button type="button"
-                    class="bg-red-600 hover:bg-red-700 p-2 text-white uppercase text-xs font-extrabold rounded-lg">Cerrar
-                    Sesión</button>
-            </div>
+                    <button type="button"
+                        class="bg-red-600 hover:bg-red-700 p-2 text-white uppercase text-xs font-extrabold rounded-lg"
+                        @click="user.logout">Cerrar
+                        Sesión</button>
+                </div>
 
-            <nav class="flex gap-2 item-center justify-end">
-                <RouterLink :to="{ name: 'my-appointment' }" class=" p-3 text-gray-200 uppercase text-xs font-black
+                <nav class="flex gap-2 item-center justify-end">
+                    <RouterLink :to="{ name: 'my-appointment' }" class=" p-3 text-gray-200 uppercase text-xs font-black
                     rounded-lg">Mis Citas</RouterLink>
 
-                <RouterLink :to="{ name: 'new-appointment' }"
-                    class="p-3 text-gray-200 uppercase text-xs font-black rounded-lg bg-blue-700">
-                    Nueva Cita
-                </RouterLink>
-            </nav>
+                    <RouterLink :to="{ name: 'new-appointment' }"
+                        class="p-3 text-gray-200 uppercase text-xs font-black rounded-lg bg-blue-700">
+                        Nueva Cita
+                    </RouterLink>
+                </nav>
 
+            </div>
         </div>
+
+        <main>
+            <RouterView />
+        </main>
     </div>
-    
-    <main>
-        <RouterView />
-    </main>
 </template>
 
 
--- src/views/appointment/MyAppointmentView.vue
+++ src/views/appointment/MyAppointmentView.vue
@@ -1,5 +1,7 @@
 <script setup>
+import { useUserStore } from '../../stores/user'
 
+const user = useUserStore()
 </script>
 
 <template>

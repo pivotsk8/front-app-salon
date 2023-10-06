eb57783810345c8145ca22c78f0e1d4899a8d2b5
piiivot <the_pivotsk8@hotmail.com>
2023-10-06 09:52:22 -0500

    feat created component of appointment

---
+++ src/components/Appointment.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <p>desde citas</p>
+    </div>
+</template>
+
--- src/views/appointment/MyAppointmentView.vue
+++ src/views/appointment/MyAppointmentView.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { useUserStore } from '../../stores/user'
-
+import Appointment from '../../components/Appointment.vue';
 const user = useUserStore()
 </script>
 
@@ -8,6 +8,16 @@ const user = useUserStore()
     <div>
         <h1 class="text-4xl font-extrabold text-white mt-10">Mis citas</h1>
         <p class="text-white text-lg my-5">A continuación podrás administrar tus próximas citas</p>
+
+        <p v-if="user.loading" class="text-white text-2xl text-center mt-5">...Cargando</p>
+        <div v-else>
+            <p v-if="user.noAppointments" class="text-white text-2xl text-center mt-5">No tienes próximas citas</p>
+
+            <div v-else class="grid grid-cols-1 gap-5 mt-10">
+                <Appointment v-for="appointment in user.userAppointments" :key="appointment._id"
+                    :appointment="appointment" />
+            </div>
+        </div>
     </div>
 </template>
 

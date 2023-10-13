d7ce7009bafcbc4b519caeed7af3f57df7c34061
piiivot <the_pivotsk8@hotmail.com>
2023-10-13 11:32:42 -0500

    feat new view for forgot password and routing

--- src/views/auth/AuthLayout.vue
+++ src/views/auth/AuthLayout.vue
@@ -1,19 +1,25 @@
 <script setup>
+import { useRoute } from 'vue-router';
+const route = useRoute()
 
 const autRoutes = [
     { name: 'register', text: 'Crear una cuenta' },
-    { name: 'login', text: 'Iniciar Sessión' }
+    { name: 'login', text: 'Iniciar Sessión' },
+    { name: 'forgot-password', text: 'Olvide mi Pasword' }
 ]
 </script>
 
 <template>
-    <div class="mx-auto lg:w-3/5 mt-20">
-        <RouterView />
+    <div>
+        <div class="mx-auto lg:w-3/5 mt-20">
+            <RouterView />
 
-        <nav class="mt-10 flex flex-col items-center space-y-5 lg:flex-row lg:justify-between lg:space-y-0">
-            <RouterLink v-for="authRoute in autRoutes" class="uppercase font-bold text-white"
-                :to="{ name: authRoute.name }">{{ authRoute.text }}</RouterLink>
-        </nav>
+            <nav class="mt-10 flex flex-col items-center space-y-5 lg:flex-row lg:justify-between lg:space-y-0">
+                <RouterLink v-for="authRoute in autRoutes" class="uppercase font-bold text-white"
+                    :to="{ name: authRoute.name }" :class="{ 'hidden': route.name === authRoute.name }">{{ authRoute.text }}
+                </RouterLink>
+            </nav>
+        </div>
     </div>
 </template>
 
---
+++ src/views/auth/ForgotPasswordView.vue
@@ -0,0 +1,24 @@
+
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <h1 class="text-6xl font-extrabold text-white text-center mt-10">Olvide mi password</h1>
+        <p class="text-2xl text-white text-center my-5">Recupera el acceso a tu cuenta</p>
+
+        <FormKit id="loginForm" type="form" :actions="false"
+            incomplete-message="No se puede enviar, revisa las notificaciones" @submit="handleSubmit">
+
+            <FormKit type="email" label="Email" name="email" placeholder="Email de Usuario" validation="requires|email"
+                :validation-messages="{
+                    required: 'El Nombre es obligatorio',
+                    email: 'Ingresa un correo valido'
+                }" />
+
+            <FormKit type="submit">Enviar Instrucciones</FormKit>
+        </FormKit>
+    </div>
+</template>
+

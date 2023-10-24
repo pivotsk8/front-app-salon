91f965dfaf3174d598bc16cc421d08635cb72403
piiivot <the_pivotsk8@hotmail.com>
2023-10-24 11:02:53 -0500

    feat final commit

--- src/views/admin/appointmentsView.vue
+++ src/views/admin/appointmentsView.vue
@@ -1,4 +1,5 @@
 <script setup>
+import AdminAppointment from '../../components/AdminAppointment.vue'
 import { useUserStore } from '../../stores/user';
 
 const user = useUserStore();
@@ -19,6 +20,11 @@ const user = useUserStore();
             <p v-if="user.noAppointments" class="text-white text-2xl text-center mt-5">
                 No tienes próximas citas
             </p>
+
+            <div v-else class="grid grid-cols-1 gap-5 mt-10">
+                <AdminAppointment v-for="appointment in user.userAppointments" :key="appointment._id"
+                    :appointment="appointment" />
+            </div>
 
         </div>
     </div>
